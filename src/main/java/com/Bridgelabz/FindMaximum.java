d4d64804ae410247311508616be029d367b3c795
supriyaphalle <supriyaphalle@gmail.com>
2020-02-28 17:32:28 +0530

    REFACTOR: created generic class to find maximum of input

--- src/main/java/com/Bridgelabz/FindMaximum.java
+++ src/main/java/com/Bridgelabz/FindMaximum.java
@@ -1,9 +1,22 @@
 package com.Bridgelabz;
 
-public class FindMaximum {
+public class FindMaximum <E extends Comparable <E>> {
+ E a;
+ E b;
+ E c;
 
+    public FindMaximum(E a, E b, E c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
 
-    public <E extends Comparable<E>> E getMaximum (E a, E b, E c){
+    public  E getMaximum(){
+        E result= getMaximum(a,b,c);
+        return result;
+    }
+
+    public static  <E extends Comparable<E>> E getMaximum (E a, E b, E c){
         E max=a;
         if(b.compareTo(max) > 0 )
             max = b;
--- src/test/java/com/Bridgelabz/maximumTest.java
+++ src/test/java/com/Bridgelabz/maximumTest.java
@@ -4,62 +4,69 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class maximumTest {
-    FindMaximum findMaximum = new FindMaximum();
 
     @Test
     public void givenNumbers_WhenProper_ShouldReturnFirstNumber() {
-
-        int max = findMaximum.getMaximum(44, 7, 11);
+        FindMaximum findMaximum = new FindMaximum(44, 7, 11);
+        int max = (int) findMaximum.getMaximum();
         Assert.assertEquals(44, max);
 
     }
 
     @Test
     public void givenNumbers_WhenProper_ShouldReturnSecondNumber() {
-        int max = findMaximum.getMaximum(44, 74, 13);
+        FindMaximum findMaximum = new FindMaximum(44, 74, 11);
+        int max = (int)findMaximum.getMaximum();
         Assert.assertEquals(74, max);
 
     }
 
     @Test
     public void givenNumbers_WhenProper_ShouldReturnThirdNumber() {
-        int max = findMaximum.getMaximum(44, 74, 112);
+        FindMaximum findMaximum = new FindMaximum(44, 7, 112);
+        int max = (int)findMaximum.getMaximum();
         Assert.assertEquals(112, max);
     }
 
     @Test
     public void givenFloatNumbers_WhenProper_ShouldReturnFirstNumber() {
-        Float max = findMaximum.getMaximum(6.1f, 2.1f, 1.1f);
+        FindMaximum findMaximum = new FindMaximum(6.1f, 2.1f, 1.1f);
+        Float max = (Float) findMaximum.getMaximum();
         Assert.assertEquals(6.1f, max, 0.1);
     }
 
     @Test
     public void givenFloatNumbers_WhenProper_ShouldReturnSecondNumber() {
-        Float max = findMaximum.getMaximum(1.1f, 8.3f, 1.1f);
+        FindMaximum findMaximum = new FindMaximum(1.1f, 8.3f, 1.1f);
+        Float max = (Float) findMaximum.getMaximum();
         Assert.assertEquals(8.3f, max, 0.1);
     }
 
     @Test
     public void givenFloatNumbers_WhenProper_ShouldReturnThirdNumber() {
-        Float max = findMaximum.getMaximum(6.1f, 2.1f, 13.9f);
+        FindMaximum findMaximum = new FindMaximum(6.1f, 2.1f, 13.9f);
+        Float max = (Float) findMaximum.getMaximum();
         Assert.assertEquals(13.9f, max, 0.1);
     }
 
     @Test
     public void givenStrings_WhenProper_ShouldReturnFirstString() {
-        String max = findMaximum.getMaximum("Zebra", "Peach", "Banana");
+        FindMaximum findMaximum = new FindMaximum("Zebra", "Peach", "Banana");
+        String max = (String) findMaximum.getMaximum();
         Assert.assertEquals("Zebra", max);
     }
 
     @Test
     public void givenStrings_WhenProper_ShouldReturnSecondString() {
-        String max = findMaximum.getMaximum("Lomesh", "Supriya", "Abhi");
+        FindMaximum findMaximum = new FindMaximum("Lomesh", "Supriya", "Abhi");
+        String max = (String) findMaximum.getMaximum();
         Assert.assertEquals("Supriya", max);
     }
 
     @Test
     public void givenStrings_WhenProper_ShouldReturnThirdString() {
-        String max = findMaximum.getMaximum("Bat", "Mobile", "Orange");
+        FindMaximum findMaximum = new FindMaximum("Bat", "Mobile", "Orange");
+        String max = (String) findMaximum.getMaximum();
         Assert.assertEquals("Orange", max);
     }
 
